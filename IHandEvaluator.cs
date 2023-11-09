ccfec95c7cab41624d54370e78cd2e43e22a0510
Mitja Busch <Mitja.Busch@wisetechglobal.com>
2023-11-09 17:12:04 +0100

    Extract Interfaces for Evaluator and HandStrength

---
+++ IHandEvaluator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SnapCall
+{
+    public interface IHandEvaluator
+    {
+        int Evaluate(IEnumerable<ICard> cards);
+    }
+}
---
+++ IHandStrength.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SnapCall
+{
+    public interface IHandStrength : IComparable<IHandStrength>
+    {
+        HandRanking HandRanking { get; set; }
+        IList<int> Kickers { get; set; }
+    }
+}
