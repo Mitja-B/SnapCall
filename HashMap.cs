1a295731a18dc5f9c5ff280b8d4a5aaad5be56ac
Mitja Busch <Mitja.Busch@wisetechglobal.com>
2023-11-09 17:14:59 +0100

    Remove unnecessary using statements

--- HashMap.cs
+++ HashMap.cs
@@ -1,11 +1,6 @@
 ﻿using ProtoBuf;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 
 namespace SnapCall
 {
